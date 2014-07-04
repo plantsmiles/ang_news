5ab601ca04c42063be0afb7080585613a5c37dea
John Samuelson <johnsamuelson@me.com>
2014-07-03 21:16:42 -0600

    show all the posts and also added a delete button

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -7,11 +7,15 @@ app.controller('PostsCtrl', function($scope) {
         title: ''
     };
 
-    $scope.submitPost = function () {
+    $scope.submitPost = function() {
         $scope.posts.push($scope.post);
         $scope.post = {
             url: 'http://',
             title: ''
         };
     };
+
+    $scope.deletePost = function(index) {
+        $scope.posts.splice(index, 1);
+    }
 });
