e5cd7249dd64f43782be76df85b17d6b9de4e1c4
John Samuelson <johnsamuelson@me.com>
2014-07-03 21:03:17 -0600

    First checkin, saving basic posts onto the $scope

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+/* global app:true */
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+]);
+
+app.config(function ($routeProvider) {$routeProvider
+    .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+});
---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,17 @@
+'use strict';
+
+app.controller('PostsCtrl', function($scope) {
+    $scope.posts = [];
+    $scope.post = {
+        url: 'http://',
+        title: ''
+    };
+
+    $scope.submitPost = function () {
+        $scope.posts.push($scope.post);
+        $scope.post = {
+            url: 'http://',
+            title: ''
+        };
+    };
+});
