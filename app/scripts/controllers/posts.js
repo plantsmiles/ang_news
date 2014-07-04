02743ab5a71553d9368810c22c24babffe3d039c
John Samuelson <johnsamuelson@me.com>
2014-07-04 13:01:11 -0600

    added styling and a nav bar, also introduction of a filter

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,12 +5,6 @@ app.controller('PostsCtrl', function($scope, Post) {
 
     $scope.post = {url: 'http://', 'title': ''};
 
-    $scope.submitPost = function () {
-        Post.create($scope.post).then(function(ref) {
-            $location.path('/posts/' + ref.name());
-        });
-    };
-
     $scope.deletePost = function (postId) {
         Post.delete(postId);
     };
